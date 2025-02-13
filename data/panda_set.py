8301cb6a431bef8918d7df9130feae70ff9f8b75
bulatko <buloch03@yandex.ru>
2025-02-13 12:13:07 +0000

    add methods descriptions

--- data/panda_set.py
+++ data/panda_set.py
@@ -1,8 +1,7 @@
 from base import Scene, Dataset
 from pandaset import DataSet
 from pandaset.sequence import Sequence
-
-
+import numpy as np
 
 class PandaSetScene(Scene):
     def __init__(self, sequence: Sequence):
@@ -10,15 +9,23 @@ class PandaSetScene(Scene):
         self.sequence = sequence
 
     def filter_points(self):
-        # TODO: filter points by mask
+        # TODO: filter 3D points of dynamic objects and ego vehicle
+        self.points = ...
+        pass
+    def make_masks(self) -> np.ndarray:
+        # TODO: make masks of dynamic objects and ego vehicle
         pass
     def load_data(self):
         # TODO: load data (points, frames) from scene
         # TODO: convert each frame to Frames class and apply mask
         self.points = ...
         self.frames = ...
-        self.masks = ...
+        self.masks = self.make_masks()
         self.cameras = ...
+
+        self.filter_points()
+
+        
 
         pass
 
